06a31f4ccb5257cb118463ec35c2dfa4d728170f
Jeff <skeezix@skeleton.org>
2014-01-15 14:58:57 -0500

    stm32: working interupt based blinker; some nasty lessons learnt, and now trying to work out how fast we can do these in reality.

---
+++ zik80/stm-vga/test/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# after "init" can do this..
+#                     -c "stm32f2x mass_erase 0" \
+
+
+openocd -f interface/stlink-v2.cfg \
+                    -f target/stm32f2x_stlink.cfg \
+                    -c "init" -c "reset init" \
+                    -c "stm32f2x mass_erase 0" \
+                    -c "flash write_image miniblink.hex" \
+                    -c "reset" \
+                    -c "shutdown" $(NULL)
--- zik80/stm-vga/test/miniblink.c
+++ zik80/stm-vga/test/miniblink.c
@@ -1,52 +1,108 @@
-/*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- * Copyright (C) 2011 Fergus Noble <fergusnoble@gmail.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
 
-static void gpio_setup(void)
-{
-	/* Enable GPIOC clock. */
-	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPCEN);
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/exti.h>
 
-	/* Set GPIO3 and GPIO4 (in GPIO port C) to 'output push-pull'. */
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO3 | GPIO4);
+#include <libopencm3/cm3/cortex.h>
+
+//#include <stm32f2xx_rcc.h>
+
+static void gpio_setup ( void ) {
+
+  /* Enable GPIOC clock. */
+  rcc_peripheral_enable_clock ( &RCC_AHB1ENR, RCC_AHB1ENR_IOPCEN );
+
+  /* Set GPIO3 to 'output push-pull'. */
+  gpio_mode_setup ( GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO3 );
+
 }
 
-int main(void)
-{
-	int i;
+static void nvic_setup(void) {
+  /* Without this the timer interrupt routine will never be called. */
+  nvic_enable_irq(NVIC_TIM2_IRQ);
+  //nvic_set_priority(NVIC_TIM2_IRQ, 1);
+}
 
-	gpio_setup();
 
-	gpio_set(GPIOC, GPIO3);
-	gpio_clear(GPIOC, GPIO4);
+static void timer2_setup ( void ) {
 
-	/* Blink the LEDs (PC3, PC4) on the board. */
-	while (1) {
-		/* Using API function gpio_toggle(): */
-		gpio_toggle(GPIOC, GPIO3);
-		gpio_toggle(GPIOC, GPIO4);
-		for (i = 0; i < 80000 /*0*/; i++)	/* Wait a bit. */
-			__asm__("nop");
-	}
+  //timer_reset ( TIM2 );
+  //timer_set_mode
+  //timer_continuous_mode ( TIM2 );
 
-	return 0;
+  /* Set timer start value. */
+  TIM_CNT(TIM2) = 1;
+
+  /* Set timer prescaler. 72MHz/1440 => 50000 counts per second. */
+  TIM_PSC(TIM2) = 2000; // 120M/2000 = 60k/second
+
+  /* End timer value. If this is reached an interrupt is generated. */
+  TIM_ARR(TIM2) = 600;
+
+  // o-scope reports:
+  // prescale 2000, 1->600 should be 100/sec; in fact, we're exactly 20ms between which is exactly 50 .. so callback is 60MHz, not 120MHz
+  // manual says:
+  //  The reference manual (see page 133) states that the GPIO is capable of:
+  //  Fast toggle capable of changing every two clock cycles
+  // --> okay so at 120MHz, the best we can do is 60MHz of GPIO. But thats different than here, where the timer seems halved..
+
+
+  /* Update interrupt enable. */
+  TIM_DIER(TIM2) |= TIM_DIER_UIE;
+
+  //timer_set_repetition_counter ( TIM2, 100 );
+
+  /* Start timer. */
+  TIM_CR1(TIM2) |= TIM_CR1_CEN;
+
+  return;
+}
+
+void tim2_isr ( void ) {
+
+
+  //TIM2_SR &= ~TIM_SR_UIF;    //clearing update interrupt flag
+  TIM_SR(TIM2) &= ~TIM_SR_UIF; /* Clear interrrupt flag. */
+
+  // ISR HAS TO DO SOMRTHING A LITTLE HEAVY
+  // https://my.st.com/public/STe2ecommunities/mcu/Lists/cortex_mx_stm32/Flat.aspx?RootFolder=/public/STe2ecommunities/mcu/Lists/cortex_mx_stm32/Problem%20with%20DMA-USART%20Rx%20on%20STM32F407VG&FolderCTID=0x01200200770978C69A1141439FE559EB459D7580009C4E14902C3CDE46A77F0FFD06506F5B&currentviews=148
+  // ie: after setting SR, you need to pause read/writes
+  // ie: if the ISR is too quick, there may be a spurious re-invocation (tail chain), so you need to do a little something to avoid cpu race condition
+  __asm__("nop");
+
+  gpio_toggle(GPIOC, GPIO3); /* LED on/off. */
+
+}
+
+
+int main ( void ) {
+
+#if 1 // go for 120MHz, built into libopencm3
+  // requires: external 8MHz crystal on pin5/6 with associated caps to ground
+  rcc_clock_setup_hse_3v3 ( &hse_8mhz_3v3 [ CLOCK_3V3_120MHZ ] );
+#endif
+
+  gpio_setup();
+  gpio_set ( GPIOC, GPIO3 );
+
+  /* Enable TIM2 clock. */
+  rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_TIM2EN);
+
+  //__enable_irq();
+  //cm_enable_interrupts();
+
+  nvic_setup();
+
+  timer2_setup();
+
+  TIM_SR(TIM2) &= ~TIM_SR_UIF; /* Clear interrrupt flag. */
+
+  while ( 1 ) {
+    __asm__("nop");
+  } // while forever
+
+  return 0;
 }
